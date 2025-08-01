92ea5b942b03f0de8445ac5394856f8590c2643a
xmanazc <anzhichao456@163.com>
2025-08-01 15:02:50 +0800

    点亮 led.

--- board_stm32/stm32f407-st-discovery/applications/main.c
+++ board_stm32/stm32f407-st-discovery/applications/main.c
@@ -17,18 +17,19 @@
 #endif /* RT_USING_NANO */
 
 /* defined the LED0 pin: PD14 */
-#define LED0_PIN    GET_PIN(D, 14)
+#define LED0_PIN    GET_PIN(E, 0)
+#define LED1_PIN    GET_PIN(E, 1)
 
 int main(void)
 {
     /* set LED0 pin mode to output */
     rt_pin_mode(LED0_PIN, PIN_MODE_OUTPUT);
+    rt_pin_mode(LED1_PIN, PIN_MODE_OUTPUT);
 
     while (1)
     {
-        rt_pin_write(LED0_PIN, PIN_HIGH);
-        rt_thread_mdelay(500);
-        rt_pin_write(LED0_PIN, PIN_LOW);
+        rt_pin_write(LED0_PIN, !rt_pin_read(LED0_PIN));
+        rt_pin_write(LED1_PIN, !rt_pin_read(LED1_PIN));
         rt_thread_mdelay(500);
     }
 
--- board_stm32/stm32f407-st-discovery/rtconfig.py
+++ board_stm32/stm32f407-st-discovery/rtconfig.py
@@ -17,7 +17,7 @@ PLATFORM    = 'gcc'
 if os.getenv('RTT_EXEC_PATH'):
     EXEC_PATH = os.getenv('RTT_EXEC_PATH')
 
-BUILD = ''
+BUILD = 'debug'
 
 PREFIX = 'arm-none-eabi-'
 CC = PREFIX + 'gcc'
